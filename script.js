1c7b262af1014cd5adda060cede5bdfbc168404c
odomin561-byte <odomin561@gmail.com>
2026-09-13 12:00:24 +0500

    Update script.js
    zamena

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ if (form) {
     submitBtn.disabled = true;
 
     // ⚠️ ВСТАВЬТЕ СЮДА ВАШ API-КЛЮЧ ИЗ ЛИЧНОГО КАБИНЕТА
-    const API_KEY = 'ВАШ_API_КЛЮЧ_ИЗ_FORMTOMAIL'; 
+    const API_KEY = 'Bearer r9hZPraD3cy1o3AX'; 
 
     try {
       const response = await fetch('https://api.formtomail.ru/send', {
